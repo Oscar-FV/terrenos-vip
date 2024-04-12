03a2d4500b03bc16b4fb645cb1715a902b9b6ff8
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-04-12 14:56:07 -0600

    fixed container for mobile

--- src/components/common/Container/Container.tsx
+++ src/components/common/Container/Container.tsx
@@ -9,7 +9,7 @@ const Container = ({children, className}: Props) =>{
 
     return(
         <>
-            <section className="h-screen xl:pl-16 xl:px-12 flex justify-center">
+            <section className="md:h-screen h-[100svh] xl:pl-16 xl:px-12 flex justify-center">
                 <div className={`max-w-screen-2xl w-full ${className}`}>
                     {children}
                 </div>
