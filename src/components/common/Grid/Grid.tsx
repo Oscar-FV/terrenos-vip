7d898bb9f49195ac7ec7efaba6bf03d4caa68461
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-04-12 14:45:46 -0600

    fixed video no playing in mobile and compressed video

--- src/components/common/Grid/Grid.tsx
+++ src/components/common/Grid/Grid.tsx
@@ -8,7 +8,7 @@ const Grid = () => {
             <div className="md:grid md:grid-cols-4 md:auto-rows-[200px] md:mt-20 md:gap-[16px] w-full h-full md:h-auto">
                 <div className="overlay absolute w-full h-full bg-black/25 md:hidden"></div>
                 <img src="/images/desarrollo-caribe.webp" className="gallery-img shadow-md border border-gray-100 col-start-2 col-span-3 hidden md:block" alt="" />
-                <video src={video} autoPlay muted loop className="gallery-img md:shadow-md md:border md:border-gray-100 md:col-span-3 md:row-span-2"></video>
+                <video src={video} playsInline autoPlay muted loop className="gallery-img md:shadow-md md:border md:border-gray-100 md:col-span-3 md:row-span-2"></video>
                 <img src="/images/flamingos.webp" className="gallery-img shadow-md border border-gray-100 row-span-2 hidden md:block" alt="" />
             </div>
         </>
