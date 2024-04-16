3ab7962900d3ed2fc1b3ada65f991024bf98d20d
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-04-15 23:24:08 -0600

    fixed hero section for mobile

--- src/components/common/Container/Container.tsx
+++ src/components/common/Container/Container.tsx
@@ -9,7 +9,7 @@ interface Props{
 const Container = ({children, className, isFirstSection = false}: Props) =>{
     return(
         <>
-            <section className={`${ isFirstSection ? "h-[100svh]" : "h-[100vh]" } md:h-screen  xl:pl-16 xl:px-12 flex justify-center`}>
+            <article className={`${ isFirstSection ? "h-[100svh]" : "h-[100vh]" } md:h-screen  xl:pl-16 xl:px-12 flex justify-center`}>
                 <div className={`w-full ${className}`}>
                     {children}
                 </div>
