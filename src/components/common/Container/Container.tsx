c1c469d4e597bafa53c429b950498664ffd2b72e
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-04-17 14:13:46 -0600

    fixed errors in container

--- src/components/common/Container/Container.tsx
+++ src/components/common/Container/Container.tsx
@@ -1,13 +1,12 @@
 import { ReactNode } from "react"
 
 interface Props{
-    isFirstSection?: boolean,
     className?: String,
     children: ReactNode,
     height?: String
 }
 
-const Container = ({children, className, isFirstSection = false, height}: Props) =>{
+const Container = ({children, className, height}: Props) =>{
     return(
         <>
             <article className={`${height} xl:pl-16 xl:px-12 flex justify-center`}>
