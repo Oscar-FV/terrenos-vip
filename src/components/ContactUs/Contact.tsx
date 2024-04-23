c84a79990435ae4d80daa8c6c8bd8135a738e056
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-04-23 16:22:10 -0600

    added links to contact

--- src/components/ContactUs/Contact.tsx
+++ src/components/ContactUs/Contact.tsx
@@ -63,18 +63,21 @@ const ContactUs = forwardRef<HTMLDivElement, Props>(({ color, text }, ref) => {
               <ContactItem
                 icon="ph:phone-call"
                 tittle="Teléfono"
-                text="Llámanos al"
+                text="Llámanos al (+52)444-801-0591"
+                goto="tel:(+52)4448010591"
               />
               <ContactItem
                 icon="ic:baseline-whatsapp"
                 tittle="WhatsApp"
                 text="Inicia un chat vía WhatsApp con nosotros"
+                goto="https://wa.me/+524448010591?text=Me%20interesa%20obtener%20información%20sobre%20los%20terrenos%20que%20tienes%20disponibles"
               />
               <ContactItem
                 icon="arcticons:zoom"
                 iconClassName="stroke-2"
                 tittle="Videollamada"
                 text="Agenda una videollamada con nosotros"
+                goto="https://calendly.com/sofia-flores-6xoe/30min"
               />
             </div>
           </div>
@@ -89,18 +92,21 @@ const ContactUs = forwardRef<HTMLDivElement, Props>(({ color, text }, ref) => {
           <ContactItem
             icon="ph:phone-call"
             tittle="Teléfono"
-            text="Llámanos al"
+            text="Llámanos al (+52)444-801-0591"
+            goto="tel:(+52)4448010591"
           />
           <ContactItem
             icon="ic:baseline-whatsapp"
             tittle="WhatsApp"
             text="Inicia un chat vía WhatsApp con nosotros"
+            goto="https://wa.me/+524448010591?text=Me%20interesa%20obtener%20información%20sobre%20los%20terrenos%20que%20tienes%20disponibles"
           />
           <ContactItem
             icon="arcticons:zoom"
             iconClassName="md:stroke-2"
             tittle="Videollamada"
             text="Agenda una videollamada con nosotros"
+            goto="https://calendly.com/sofia-flores-6xoe/30min"
           />
         </div>
       </div>
