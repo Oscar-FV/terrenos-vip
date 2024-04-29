a42ab04a3eab6ae290d2e2e5efd4c5f94ff9f89f
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-04-29 13:28:43 -0600

    added react router, error page, fixed aopacity for hero section, optimezed hero video

--- src/components/common/Grid/Grid.tsx
+++ src/components/common/Grid/Grid.tsx
@@ -4,11 +4,10 @@ import video from "../../../assets/images/peninsula.mp4";
 const Grid = () => {
   return (
     <>
-      <div className="h-full w-full md:mt-20 md:grid md:h-auto md:auto-rows-[200px] md:grid-cols-4 md:gap-[16px]">
-        <div className="overlay absolute h-full w-full bg-black/50 md:hidden"></div>
+      <div className="h-full w-full bg-black md:mt-20 md:grid md:h-auto md:auto-rows-[200px] md:grid-cols-4 md:gap-[16px] md:bg-transparent">
         <img
           src="/images/desarrollo-caribe.webp"
-          className="gallery-img col-span-3 col-start-2 hidden border border-gray-100 shadow-md md:block animate-fade-left animate-delay-100"
+          className="gallery-img col-span-3 col-start-2 hidden animate-fade-left border border-gray-100 shadow-md animate-delay-100 md:block"
           alt=""
         />
         <video
@@ -17,11 +16,11 @@ const Grid = () => {
           autoPlay
           muted
           loop
-          className="gallery-img md:col-span-3 md:row-span-2 md:border md:border-gray-100 md:shadow-md md:animate-fade-down md:animate-delay-200"
+          className="gallery-img opacity-35 md:col-span-3 md:row-span-2 md:animate-fade-down md:border md:border-gray-100 md:opacity-100 md:shadow-md md:animate-delay-200"
         ></video>
         <img
           src="/images/flamingos.webp"
-          className="gallery-img row-span-2 hidden border border-gray-100 shadow-md md:block animate-fade-up animate-delay-150"
+          className="gallery-img row-span-2 hidden animate-fade-up border border-gray-100 shadow-md animate-delay-150 md:block"
           alt=""
         />
       </div>
