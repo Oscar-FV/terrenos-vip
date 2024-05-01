5fe1cd7ed3f29c62e3c973461264f45bdc85f14f
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-04-30 19:34:31 -0600

    added google maps api

--- src/components/VenueListing/VenueListing.tsx
+++ src/components/VenueListing/VenueListing.tsx
@@ -17,7 +17,7 @@ const VenueListing = () => {
             <VenueItem/>
         </article>
 
-        <figure className="h-full w-full border border-blue-600">
+        <figure className="h-full w-full rounded-lg overflow-hidden">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d323065.9271595519!2d-102.1960558515789!3d21.885426432680667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842a150f1d5ff5d5%3A0xf29d198bed1ac8a!2sCiudad%20Maderas%20Aguascalientes!5e0!3m2!1ses-419!2smx!4v1714520662769!5m2!1ses-419!2smx" loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="w-full h-full"></iframe>
         </figure>
       </Container>
